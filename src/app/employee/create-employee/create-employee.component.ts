3f58bbda5467983563d1cb5e1b9ae9cacac2bd31
gaurav rattan <gauravrattan76@gmail.com>
2019-02-23 18:11:43 +0530

    rectiying looping logic

--- src/app/employee/create-employee/create-employee.component.ts
+++ src/app/employee/create-employee/create-employee.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, FormBuilder, Validators, AbstractControl } from '@angular/forms';
+import { Key } from 'protractor';
 
 @Component({
   selector: 'app-create-employee',
@@ -21,73 +22,29 @@ export class CreateEmployeeComponent implements OnInit {
       })
     });
 
-
-    // this.employeeForm.get("fullName").valueChanges.subscribe(
-    //   data => console.log(data)
+    // this.employeeForm.valueChanges.subscribe(
+    //   //data => console.log(JSON.stringify(data))
     // );
-
-
-    this.employeeForm.valueChanges.subscribe(
-      data => console.log(JSON.stringify(data))
-    );
   }
-
-  // ngOnInit() {
-  //   this.employeeForm = new FormGroup({
-  //     fullName: new FormControl(),
-  //     email: new FormControl(),
-  //     skills: new FormGroup({
-  //       skill: new FormControl(),
-  //       experience: new FormControl(),
-  //       proficiency: new FormControl()
-  //     })
-  //   })
-  // }
 
   onSubmit() {
     console.log(this.employeeForm.value);
   }
 
-  // loadData(){
-  //   this.employeeForm.setValue({
-  //     fullName:"Gaurav Rattan",
-  //     email:"gauravrattan76@gmail.com",
-  //     skills:{
-  //       skill:"Angular 6",
-  //       experience: 5,
-  //       proficiency:"Expert"
-  //     }
-  //   });
-  // }
-
-  logKeyValuePairs(group: FormGroup): void {
-    // loop through each key in the FormGroup
-    Object.keys(group.controls).forEach((key: string) => {
-      // Get a reference to the control using the FormGroup.get() method
+  logKeyValuePairs(group: FormGroup) {
+    Object.keys(group.controls).forEach((key) => {
       const abstractControl = group.get(key);
-      // If the control is an instance of FormGroup i.e a nested FormGroup
-      // then recursively call this same method (logKeyValuePairs) passing it
-      // the FormGroup so we can get to the form controls in it
       if (abstractControl instanceof FormGroup) {
-        this.logKeyValuePairs(abstractControl);
-        // If the control is not a FormGroup then we know it's a FormControl
-      } else {
-        console.log('Key = ' + key + ' && Value = ' + abstractControl.value);
+        this.logKeyValuePairs(abstractControl)
+      }
+      else {
+        console.log("Key  " + key + " value  " + abstractControl.value);
       }
     });
   }
 
   loadData() {
     this.logKeyValuePairs(this.employeeForm);
-    // this.employeeForm.patchValue({
-    //   fullName: "Gaurav Rattan",
-    //   email: "gauravrattan76@gmail.com",
-    //   // skills:{
-    //   //   skill:"Angular 6",
-    //   //   experience: 5,
-    //   //   proficiency:"Expert"
-    //   // }
-    // });
   }
 
 }
